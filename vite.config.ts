c5dff320f82dc98df9f06fbb49496c2b69f49af8
Tran Hoang Anh Tu <trananhtu1112003@gmail.com>
2025-05-08 12:29:12 +0700

    config: tsconfig alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    }
+      '@': '/src',
+    },
   },
-  plugins: [react(), tailwindcss()],
-  server: {
-    port: 2025,
-    open: true
-  }
 })
